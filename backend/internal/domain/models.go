e795637d90612a693938fd661beef8a65db12aea
Jaswinder Singh <jaswinder@fountane.com>
2025-07-06 21:33:51 +0530

    Refactor PriceData model and enhance RoundManager for improved data handling
    - Updated PriceData model to use time.Time instead of Unix timestamp for better time representation.
    - Modified RoundManager to handle chart and hourly data channels, improving data loading during phase transitions.
    - Enhanced logging for data loading processes and added functionality to manage live phase data broadcasting.

--- backend/internal/domain/models.go
+++ backend/internal/domain/models.go
@@ -1,17 +1,19 @@
 package domain
 
+import "time"
+
 type Player struct {
 	Id       string `json:"id"`
 	Username string `json:"username"`
 }
 
 type PriceData struct {
-	Time   int64   `json:"time"`
-	Open   float64 `json:"open"`
-	High   float64 `json:"high"`
-	Low    float64 `json:"low"`
-	Close  float64 `json:"close"`
-	Volume float64 `json:"volume"`
+	Time   time.Time `json:"time"`
+	Open   float64   `json:"open"`
+	High   float64   `json:"high"`
+	Low    float64   `json:"low"`
+	Close  float64   `json:"close"`
+	Volume float64   `json:"volume"`
 }
 
 type Phase string
--- backend/internal/service/round_manager.go
+++ backend/internal/service/round_manager.go
@@ -12,18 +12,21 @@ type RoundManager struct {
 	hub            *Hub
 	marketService  *MarketService
 	phase          domain.Phase
-	phaseCountDown uint
+	phaseCountDown int
+	chartDataChan  chan []domain.PriceData
 	chartData      []domain.PriceData
+	hourlyDataChan chan []domain.PriceData
 	hourlyData     []domain.PriceData
 }
 
 const (
-	LobbyDuration    = 15 * time.Second
-	LiveDuration     = 45 * time.Second
-	CooldownDuration = 10 * time.Second
-	RoundDuration    = LobbyDuration + LiveDuration + CooldownDuration
-	BroadcastTick    = 500 * time.Millisecond
-	Ticker           = "X:BTCUSD"
+	LobbyDuration     = 15 * time.Second
+	LiveDuration      = 45 * time.Second
+	CooldownDuration  = 10 * time.Second
+	HourlyDataForDays = 10
+	RoundDuration     = LobbyDuration + LiveDuration + CooldownDuration
+	LivePhaseTick     = LiveDuration / (HourlyDataForDays * 24)
+	Ticker            = "X:BTCUSD"
 )
 
 func NewRoundManager(hub *Hub, marketService *MarketService) *RoundManager {
@@ -36,6 +39,12 @@ func (r *RoundManager) Run() {
 	defer timer.Stop()
 
 	for range timer.C {
+		if r.phase == "" {
+			log.Println("--- Starting Round Manager ---")
+			r.transitionToLobby()
+			continue
+		}
+
 		r.phaseCountDown--
 
 		if r.phaseCountDown <= 0 {
@@ -55,23 +64,31 @@ func (r *RoundManager) Run() {
 func (r *RoundManager) transitionToLive() {
 	log.Println("--- Transitioning to Live Phase ---")
 	r.phase = domain.Live
-	r.phaseCountDown = uint(LiveDuration.Seconds())
+	r.phaseCountDown = int(LiveDuration.Seconds())
+	log.Println(r.chartDataChan, r.hourlyDataChan)
+	r.chartData = <-r.chartDataChan
+	r.hourlyData = <-r.hourlyDataChan
+
+	log.Printf("Loaded %d daily chart data and %d hourly data", len(r.chartData), len(r.hourlyData))
 
 	data := map[string]any{
 		"phase":        r.phase,
 		"phaseEndTime": time.Now().Add(LiveDuration).Unix(),
+		"chartData":    r.chartData,
 	}
 	msg := WsMessage{
 		Type: WsMessageTypeRoundStatus,
 		Data: data,
 	}
 	r.hub.Broadcast <- msg
+
+	go r.runLivePhase()
 }
 
 func (r *RoundManager) transitionToCooldown() {
 	log.Println("--- Transitioning to Cooldown Phase ---")
 	r.phase = domain.Closed
-	r.phaseCountDown = uint(CooldownDuration.Seconds())
+	r.phaseCountDown = int(CooldownDuration.Seconds())
 
 	data := map[string]any{
 		"phase":         r.phase,
@@ -87,39 +104,40 @@ func (r *RoundManager) transitionToCooldown() {
 func (r *RoundManager) transitionToLobby() {
 	log.Println("--- Transitioning to Lobby Phase ---")
 
-	chartDataChan := make(chan []domain.PriceData)
-	hourlyDataChan := make(chan []domain.PriceData)
 	randomDecrease := -3 - int(rand.Float64()*10)
 	go func() {
 		from := time.Now().AddDate(-2, 0, 0) // 2 years ago
 		to := time.Now().AddDate(0, randomDecrease, 0)
+		log.Printf("Loading daily chart data from %s to %s", from, to)
 
 		chartData, err := r.marketService.LoadPriceData(context.Background(), Ticker, from, to, "day")
 		if err != nil {
 			log.Println("Error loading price data:", err)
-			chartDataChan <- nil
+			r.chartDataChan <- nil
 			return
 		}
 
-		chartDataChan <- chartData
-
+		log.Printf("Loaded %d daily chart data points", len(chartData))
+		r.chartDataChan <- chartData
 	}()
 
 	go func() {
 		from := time.Now().AddDate(0, randomDecrease, 1)
-		to := time.Now().AddDate(0, randomDecrease, 10)
+		to := time.Now().AddDate(0, randomDecrease, HourlyDataForDays)
+		log.Printf("Loading hourly chart data from %s to %s", from, to)
 
 		hourlyData, err := r.marketService.LoadPriceData(context.Background(), Ticker, from, to, "hour")
 		if err != nil {
 			log.Println("Error loading hourly price data:", err)
-			hourlyDataChan <- nil
+			r.hourlyDataChan <- nil
 			return
 		}
-		hourlyDataChan <- hourlyData
+		log.Printf("Loaded %d hourly chart data points", len(hourlyData))
+		r.hourlyDataChan <- hourlyData
 	}()
 
 	r.phase = domain.Lobby
-	r.phaseCountDown = uint(LobbyDuration.Seconds())
+	r.phaseCountDown = int(LobbyDuration.Seconds())
 
 	data := map[string]any{
 		"phase":         r.phase,
@@ -130,6 +148,41 @@ func (r *RoundManager) transitionToLobby() {
 		Data: data,
 	}
 	r.hub.Broadcast <- msg
-	r.chartData = <-chartDataChan
-	r.hourlyData = <-hourlyDataChan
+}
+
+func (r *RoundManager) runLivePhase() {
+	log.Println("--- Running Live Phase ---")
+	if len(r.hourlyData) == 0 {
+		log.Println("no hourly data to broadcast")
+		return
+	}
+	ticker := time.NewTicker(LivePhaseTick)
+	defer ticker.Stop()
+
+	i := 0
+	for range ticker.C {
+		if i >= len(r.hourlyData) {
+			break
+		}
+		lastChartData := r.chartData[len(r.chartData)-1]
+		priceData := r.hourlyData[i]
+
+		if lastChartData.Time.Day() != priceData.Time.Day() {
+			r.chartData = append(r.chartData, priceData)
+		} else {
+			lastChartData.High = max(lastChartData.High, priceData.High)
+			lastChartData.Low = min(lastChartData.Low, priceData.Low)
+			lastChartData.Close = priceData.Close
+			lastChartData.Volume += priceData.Volume
+			r.chartData[len(r.chartData)-1] = priceData
+		}
+
+		msg := WsMessage{
+			Type: WsMessageTypePriceData,
+			Data: priceData,
+		}
+		r.hub.Broadcast <- msg
+		i++
+	}
+	log.Println("--- Live Phase Finished ---")
 }
